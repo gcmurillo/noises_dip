a468d5d9a80c390b21d45ab14ebc146e5d869e5a
gcmurillo <gcmurillo@outlook.com>
2019-01-09 02:33:30 -0500

    add medio aritmetico

--- filters.py
+++ filters.py
@@ -95,10 +95,26 @@ def alpha_trimmed_filter(image, mask_size=3, T=3):
     return new_image
 
 
+def medio_filter(image, mask_size=3):
+    temp_image = np.float64(np.copy(image))
+    new_image = np.float64(np.copy(image))
+
+    h = temp_image.shape[0]
+    w = temp_image.shape[1]
+
+    if len(temp_image.shape) == 2:  # si es de un solo canal
+        for i in range(h - 1):
+            for j in range(w - 1):
+                mask = temp_image[i:i+mask_size, j:j+mask_size]
+                prom = mask.mean()
+                new_image[i+(mask_size//2), j+(mask_size//2)] = prom
+
+    return new_image
+
 
 image = cv2.imread('image1.jpg',0)
 noisy = noises.gaussian_noise(image)
-restaured = alpha_trimmed_filter(noisy)
+restaured = medio_filter(noisy)
 plt.subplot(1,3,1),plt.imshow(image, cmap = 'gray')
 plt.title('Source'), plt.xticks([]), plt.yticks([])
 plt.subplot(1,3,2),plt.imshow(noisy, cmap = 'gray')
