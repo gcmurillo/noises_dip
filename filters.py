631867617eb87a9cc5b47ca09515ca39ffc20ef0
gcmurillo <gcmurillo@outlook.com>
2019-01-09 01:28:42 -0500

    add filter minimum

--- filters.py
+++ filters.py
@@ -20,13 +20,43 @@ def maximun_filter(image, mask_size=3):
                 mask = temp_image[i:i+mask_size, j:j+mask_size]
                 maximum = np.max(mask)
                 new_image[i+(mask_size//2), j+(mask_size//2)] = maximum
+    # else:
+    #     for i in range(h - 1):
+    #         for j in range(w - 1):
+    #             mask_0 = temp_image[i:i+mask_size, j:j+mask_size,0]
+    #             maximum_0 = np.max(mask_0)
+    #             mask_1 = temp_image[i:i+mask_size, j:j+mask_size,1]
+    #             maximum_1 = np.max(mask_1)
+    #             mask_2 = temp_image[i:i+mask_size, j:j+mask_size,2]
+    #             maximum_2 = np.max(mask_2)
+    #             new_image[i+(mask_size//2), j+(mask_size//2), 0] = maximum_0
+    #             new_image[i+(mask_size//2), j+(mask_size//2), 1] = maximum_1
+    #             new_image[i+(mask_size//2), j+(mask_size//2), 2] = maximum_2
 
     return new_image
 
 
+def minimun_filter(image, mask_size=3):
+    temp_image = np.float64(np.copy(image))
+    new_image = np.float64(np.copy(image))
+
+    h = temp_image.shape[0]
+    w = temp_image.shape[1]
+
+    if len(temp_image.shape) == 2:  # si es de un solo canal
+        for i in range(h - 1):
+            for j in range(w - 1):
+                mask = temp_image[i:i+mask_size, j:j+mask_size]
+                maximum = np.min(mask)
+                new_image[i+(mask_size//2), j+(mask_size//2)] = maximum
+
+    return new_image
+
+
+
 image = cv2.imread('image1.jpg',0)
-noisy = noises.pepper_noise(image)
-restaured = maximun_filter(noisy)
+noisy = noises.salt_noise(image)
+restaured = minimun_filter(noisy)
 plt.subplot(1,3,1),plt.imshow(image, cmap = 'gray')
 plt.title('Source'), plt.xticks([]), plt.yticks([])
 plt.subplot(1,3,2),plt.imshow(noisy, cmap = 'gray')
