fd3d663cc32f640fe68a1dfd4caf3d960f5f88f1
gcmurillo <gcmurillo@outlook.com>
2019-01-09 00:42:14 -0500

    noises

---
+++ noises.py
@@ -0,0 +1,71 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import random
+
+def gaussian_noise(image_in, noise_sigma=35):
+    temp_image = np.float64(np.copy(image_in))
+
+    h = temp_image.shape[0]
+    w = temp_image.shape[1]
+    noise = np.random.randn(h, w) * noise_sigma
+
+    noisy_image = np.zeros(temp_image.shape, np.float64)
+    if len(temp_image.shape) == 2:
+        noisy_image = temp_image + noise
+    else:
+        noisy_image[:,:,0] = temp_image[:,:,0] + noise
+        noisy_image[:,:,1] = temp_image[:,:,1] + noise
+        noisy_image[:,:,2] = temp_image[:,:,2] + noise
+
+    return noisy_image
+
+
+def salt_peeper_noise(image, prob=0.05):
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = image[i][j]
+    return output
+
+
+def salt_noise(image, prob=0.05):
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = image[i][j]
+    return output
+
+
+def pepper_noise(image, prob=0.05):
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            else:
+                output[i][j] = image[i][j]
+    return output
+
+
+image = cv2.imread('image1.jpg')
+noise = pepper_noise(image)
+cv2.imwrite('result.jpg', noise)
+plt.imshow(noise)
+plt.show()
+# cv2.imshow('ggg', noise)
+# cv2.waitKey(0)
